2076056ebe3390561dc65d4237e991ff484ca266
Les Aker <me@lesaker.org>
2024-12-03 22:06:27 -0500

    update to ledgergraph

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
-puppet-ledgerdb
+puppet-ledgergraph
 ===========
 
-[![Puppet Forge](https://img.shields.io/puppetforge/v/halyard/ledgerdb.svg)](https://forge.puppetlabs.com/halyard/ledgerdb)
-[![GitHub Workflow Status](https://img.shields.io/github/actions/workflow/status/halyard/puppet-ledgerdb/build.yml?branch=main)](https://github.com/halyard/puppet-ledgerdb/actions)
+[![Puppet Forge](https://img.shields.io/puppetforge/v/halyard/ledgergraph.svg)](https://forge.puppetlabs.com/halyard/ledgergraph)
+[![GitHub Workflow Status](https://img.shields.io/github/actions/workflow/status/halyard/puppet-ledgergraph/build.yml?branch=main)](https://github.com/halyard/puppet-ledgergraph/actions)
 [![MIT Licensed](http://img.shields.io/badge/license-MIT-green.svg?style=flat)](https://tldrlegal.com/license/mit-license)
 
-Module to configure ledgerdb
+Module to configure ledgergraph
 
 ## Usage
 
 ```puppet
-include ledgerdb
+include ledgergraph
 ```
 ## License
 
-ledgerdb is released under the MIT License. See the bundled LICENSE file for details.
+ledgergraph is released under the MIT License. See the bundled LICENSE file for details.
 
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,28 +1,20 @@
-# @summary Configure Ledger DB instance
+# @summary Configure Ledgergraph instance
 #
 # @param datadir sets where the data is persisted
-# @param influx_url sets the InfluxDB hostname
-# @param influx_org sets the InfluxDB Organization
-# @param influx_token sets the credential to use for metric submission
-# @param influx_bucket sets the InfluxDB bucket
 # @param ledger_repo is the git repo for ledger data
 # @param ledger_ssh_key is the ssh key to use to update the repo
 # @param ledger_file is the main ledger file to load, relative to the repo root
-# @param version sets the ledgerdb tag to use
-# @param user sets the user to run ledgerdb as
+# @param version sets the ledgergraph tag to use
+# @param user sets the user to run ledgergraph as
 # @param bootdelay sets how long to wait before first run
 # @param frequency sets how often to run updates
-class ledgerdb (
+class ledgergraph (
   String $datadir,
-  String $influx_url,
-  String $influx_org,
-  String $influx_token,
-  String $influx_bucket,
   String $ledger_repo,
   String $ledger_ssh_key,
   String $ledger_file = 'core.ldg',
-  String $version = 'v0.0.13',
-  String $user = 'ledgerdb',
+  String $version = 'v0.1.0',
+  String $user = 'ledgergraph',
   String $bootdelay = '300',
   String $frequency = '300'
 ) {
@@ -64,7 +56,7 @@ class ledgerdb (
 
   file { "${datadir}/config.yaml":
     ensure  => file,
-    content => template('ledgerdb/config.yaml.erb'),
+    content => template('ledgergraph/config.yaml.erb'),
     group   => $user,
     mode    => '0640',
   }
@@ -77,26 +69,21 @@ class ledgerdb (
     default   => 'error',
   }
 
-  $binfile = '/usr/local/bin/ledgerdb'
-  $filename = "ledgerdb_${downcase($facts['kernel'])}_${arch}"
-  $url = "https://github.com/akerl/ledgerdb/releases/download/${version}/${filename}"
+  $binfile = '/usr/local/bin/ledgergraph'
+  $filename = "ledgergraph_${downcase($facts['kernel'])}_${arch}"
+  $url = "https://github.com/akerl/ledgergraph/releases/download/${version}/${filename}"
 
-  exec { 'download ledgerdb':
+  exec { 'download ledgergraph':
     command => "/usr/bin/curl -sLo '${binfile}' '${url}' && chmod a+x '${binfile}'",
     unless  => "/usr/bin/test -f ${binfile} && ${binfile} version | grep '${version}'",
   }
 
-  file { '/etc/systemd/system/ledgerdb.service':
+  file { '/etc/systemd/system/ledgergraph.service':
     ensure  => file,
-    content => template('ledgerdb/ledgerdb.service.erb'),
+    content => template('ledgergraph/ledgergraph.service.erb'),
   }
 
-  file { '/etc/systemd/system/ledgerdb.timer':
-    ensure  => file,
-    content => template('ledgerdb/ledgerdb.timer.erb'),
-  }
-
-  ~> service { 'ledgerdb.timer':
+  ~> service { 'ledgergraph':
     ensure => running,
     enable => true,
   }
